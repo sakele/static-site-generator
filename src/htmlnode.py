5042d5c881a43120f12dc270e5aa60f80efbf7d1
sakele <107649303+sakele@users.noreply.github.com>
2026-06-21 23:59:42 -0700

    Add and test LeafNode subclass of HTMLNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,7 +1,7 @@
 class HTMLNode:
     def __init__(
         self,
-        tag: str | None =None,
+        tag: str | None = None,
         value: str | None = None,
         children: list["HTMLNode"] | None = None,
         props: dict[str, str] | None = None
@@ -12,6 +12,9 @@ class HTMLNode:
         self.children = children
         self.props = props
     
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
     def to_html(self) -> str:
         raise NotImplementedError("to_html method not implemented")
     
@@ -21,5 +24,27 @@ class HTMLNode:
             string += f' {prop}="{self.props[prop]}"'
         return string
     
+            
+class LeafNode(HTMLNode):
+    def __init__(
+        self,
+        tag: str | None,
+        value: str,
+        props: dict[str, str] | None = None
+    ) -> None:
+        
+        super().__init__(tag, value, None, props)
+
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("invalid HTML: no value")
+        if not self.tag:
+            return self.value
+        if not self.props:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        
+        html = f"<{self.tag}" + self.props_to_html() + f">{self.value}</{self.tag}>"
+        return html
