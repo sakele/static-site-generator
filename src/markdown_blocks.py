1a2b0b89b561deaea714245a81091077245b29a7
sakele <107649303+sakele@users.noreply.github.com>
2026-06-25 19:14:49 -0700

    Add and test BlockType enum and block_to_block_type function in markdown_blocks.py

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,14 @@
+import re
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    OLIST = "ordered_list"
+    ULIST = "unordered_list"
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = markdown.split("\n\n")
     blocks = list(map(str.strip, blocks))
@@ -5,3 +16,30 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         if block.isspace() or block == "":
             blocks.remove(block)
     return blocks
+
+def block_to_block_type(block: str) -> BlockType:
+    lines = block.split("\n")
+    
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and block.startswith("```\n") and lines[-1].endswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
+    
