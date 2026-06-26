e8e1ed62d51431f5b29d1580d85ef5d7cdfae796
sakele <107649303+sakele@users.noreply.github.com>
2026-06-25 17:54:06 -0700

    Add and test text_to_textnodes function in inline_markdown.py

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,6 +1,15 @@
 import re
 from textnode import TextNode, TextType
 
+def text_to_textnodes(text: str) -> list[TextNode]:
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
     new_nodes = []
     for node in old_nodes:
@@ -76,3 +85,4 @@ def extract_markdown_images(text: str) -> list[tuple]:
 def extract_markdown_links(text: str) -> list[tuple]:
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
+
