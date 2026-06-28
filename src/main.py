b047eadd22283b5d621b1358d3c1d4ccd684d2d8
sakele <107649303+sakele@users.noreply.github.com>
2026-06-27 22:28:42 -0700

    Add copy_dir function to recursively copy a source directory and its contents to a destination; add static directory for website content and styling

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
+import os, shutil
 from textnode import TextNode, TextType
 
 def main():
     text_node = TextNode("This is some anchor text", TextType.LINK, "https://boot.dev")
     print(text_node)
+    copy_dir("./static", "./public")
+
+def copy_dir(source, destination):
+    if not os.path.exists(source):
+        raise Exception("path to source directory does not exist")
+    if os.path.exists(destination):
+        print(f"REMOVING DIRECTORY: {destination}")
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+    items = os.listdir(source)
+    for item in items:
+        source_subpath = os.path.join(source, item)
+        destination_subpath = os.path.join(destination, item)
+        if os.path.isfile(source_subpath):
+            print(f"COPYING ITEM: {source_subpath} to {destination_subpath}")
+            shutil.copy(source_subpath, destination)
+        else:
+            copy_dir(source_subpath, destination_subpath)
+        
+    
 
 main()
